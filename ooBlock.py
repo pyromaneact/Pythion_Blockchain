1ec7586b546d18a8525e2a659a177a5ed795d6c9
Cyber_Coo <40432224@live.napier.ac.uk>
2022-01-04 12:54:23 +0000

    working fitness and hash functions

--- ooBlock.py
+++ ooBlock.py
@@ -6,51 +6,75 @@ class Chain:
     
     chains=0
     
-    def __init__(self, chain, fittness='''self.__fitness__('0')'''):
+    def __init__(self, chain, fittness='''self.__fitness__('0')''', hashing = ('''self.__hashing__(''')):
         self.chain=chain
         self.length=len(chain)
         self.fitnessFunction=fittness
+        self.hashingFunction=hashing
         Chain.chains += 1
 
     def addBlock(self, data):
         timestamp = self.__time__()
+        fitness={'self':self}
+        exec('check =' + self.fitnessFunction, globals(), fitness)
         if self.length == 0:
             lastHash = '0'
         else:
-            lastHash = self.__hashing__(self.chain[-1])
-        self.chain.append(block(self, data, timestamp, lastHash, self.fitnessFunction))
+            hashed={}
+            exec('Hash = ' + self.hashingFunction + 'self.chain[-1])', globals(), hashed)
+            lastHash=hashed['Hash']
+        self.chain.append(block(self.chain, data, timestamp, lastHash, fitness['check'], self.hashingFunction))
 
     def __time__(self):
         utc_time = datetime.utcnow()
         utc_timestamp = utc_time.timestamp()
         return utc_timestamp
 
+
     def __hashing__(self, unencrypted_string):
         unencrypted_bytes = unencrypted_string.__encode__()
         encrypted_bytes = hashlib.sha256(unencrypted_bytes)
         encrypted_block = encrypted_bytes.hexdigest()
         return encrypted_block
+
+    def __check_chain__(self):
+        pass
+    
+    def __get_partual_chain__(self, start, end):
+        return self.chain[start,end]
+    
+    def __get_chain__(self):
+        return self.chain
     
     def __fitness__(self, value):
-        return str(value)
+        fit=self.__get_chain__()
+        if len(fit)<1:
+            return '0'
+        else:
+            return fit[-1][0]
+
+
+   
 
 class block(Chain):
-    def __init__(self, thechain, data,time,previousHash,fitness):
+    def __init__(self, thechain, data,time,previousHash,fitness, hashing):
         self.data = data
+        self.thechain = thechain
         self.time = time
         self.previousHash = previousHash
         self.fitness = fitness
+        self.hashing = hashing
         self.nonce = 0
-        self.__generateNonce__(thechain)
+        self.__generateNonce__()
 
-    def __generateNonce__( self, thechain):
+    def __generateNonce__(self):
         fitness={'self':self}
-        exec('check =' + self.fitness, globals(), fitness)
-        hashed_block = thechain.__hashing__(self)
-        while (hashed_block[0:len(fitness['check'])] != fitness['check']):
+        self.fitness
+        exec('hashed_block =' + self.hashing + 'self)', globals(), fitness)
+        while (fitness['hashed_block'][0:len(self.fitness)] != self.fitness):
             new_nonce = random.randint(0, 2147483647)
             self.nonce = new_nonce
-            hashed_block = thechain.__hashing__(self)
+            exec('hashed_block =' + self.hashing + 'self)', globals(), fitness)
     
     def __encode__(self):
         data = ''
@@ -60,6 +84,8 @@ class block(Chain):
         data += str(self.fitness)
         data += str(self.nonce)
         return bytes(data, 'utf-8')
+
+
 
 
 def main():
