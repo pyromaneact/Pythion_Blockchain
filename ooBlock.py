4d893969680fda8fea3da91f4e8e5f3cf3fefeb5
Cyber_Coo <40432224@live.napier.ac.uk>
2022-01-10 22:35:35 +0000

    fitnessValue
    added fitness value

--- ooBlock.py
+++ ooBlock.py
@@ -6,24 +6,27 @@ class Chain:
     
     chains=0
     
-    def __init__(self, chain, fittness='''self.__fitness__('0')''', hashing = ('''self.__hashing__('''), notTemp=1):
-        self.chain=chain
-        self.length=len(chain)
-        self.fitnessFunction=fittness
-        self.hashingFunction=hashing
+    def __init__(self, chain = [], fittness='''self.__fitness_function__()''', fittnessValue = '''self.__fitness_value__()''', hashing = ('''self.__hashing__('''), byte=b'' ,notTemp=1):
         Chain.chains += notTemp
+        self.chain=chain
+        if  byte == b'':
+            self.fitnessFunction=fittness
+            self.hashingFunction=hashing
+            self.fittnessValue=fittnessValue
+        else:
+            self.__get_chain_from_bytes__(byte)
 
     def addBlock(self, data):
         timestamp = self.__time__()
         fitness={'self':self}
         exec('check =' + self.fitnessFunction, globals(), fitness)
-        if self.length == 0:
+        if len(self.chain) == 0:
             lastHash = '0'
         else:
-            hashed={}
+            hashed={'self':self}
             exec('Hash = ' + self.hashingFunction + 'self.chain[-1])', globals(), hashed)
             lastHash=hashed['Hash']
-        self.chain.append(block(self.chain, data, timestamp, lastHash, fitness['check'], self.hashingFunction))
+        self.chain.append(block(data, timestamp, lastHash, fitness['check'], self.hashingFunction))
 
     def __time__(self):
         utc_time = datetime.utcnow()
@@ -41,7 +44,7 @@ class Chain:
         valid = self.chain[-1].__checkBlock__()
         if valid == 1:
             if len(self.chain) !=1:
-                tempchain=Chain(self.chain[0:-1], self.fitnessFunction,self.hashingFunction,0).__check_chain__()
+                tempchain=Chain(chain = self.chain[0:-1], fittness = self.fitnessFunction, fittnessValue = self.fittnessValue,  hashing = self.hashingFunction,notTemp = 0).__check_chain__()
                 return tempchain
             else:
                 return 1
@@ -51,29 +54,72 @@ class Chain:
     def __get_partual_chain__(self, start, end):
         return self.chain[start,end]
     
-    def __get_chain__(self):
-        return self.chain
+    def __get_chain_bytes__(self):
+        byte=b''
+        byte += bytes(self.fitnessFunction + '"], ["', 'utf-8')
+        byte += bytes(self.fittnessValue + '"], ["', 'utf-8')
+        byte += bytes(self.hashingFunction + '"], ["', 'utf-8')
+        for ablock in self.chain:
+            byte += ablock.__encode__()
+        return byte
+
+
+    def __get_chain_from_bytes__(self, byte):
+        string = byte.decode("utf-8")
+        chainparts = string.split('"], ["')
+        self.fitnessFunction = chainparts[0]
+        self.fittnessValue = chainparts[1]
+        self.hashingFunction = chainparts[2]
+        blocks = chainparts[3][0:-1].split('";; "')
+        for each in blocks:
+            items = each.split('", "')
+            fitness={'self':self}
+            exec('check =' + self.fitnessFunction, globals(), fitness)
+            self.chain.append(block( items[0], items[1], items[2], fitness['check'], self.hashingFunction))
+            
+        
     
-    def __fitness__(self, value):
-        fit=self.__get_chain__()
+    def __fitness_function__(self):
+        fit=self.chain
         if len(fit)<1:
             return '0'
         else:
-            return fit[-1][0]
+            return fit[-1].previousHash[0]
+
+    def __fitness_value__(self):
+        return len(self.chain)
+    
+    
+    def best_chain(self, chain2):
+        chain1valid = self.__check_chain__()
+        chain2valid = chain2.__check_chain__()
+        if chain2valid and chain1valid:
+            chain1value = self.__fitness_value__()
+            chain2value = chain2.__fitness_value__()
+            if chain1value < chain2value:
+                return chain2
+            else:
+                 return self
+        if chain1valid == 1:
+            return self
+        elif chain2valid == 1:
+            return chain2
+        else:
+            return 0
 
 
-   
+
 
 class block(Chain):
-    def __init__(self, thechain, data,time,previousHash,fitness, hashing):
+    def __init__(self, data,time,previousHash,fitness, hashing, nounce=-1):
         self.data = data
-        self.thechain = thechain
         self.time = time
         self.previousHash = previousHash
         self.fitness = fitness
         self.hashing = hashing
-        self.nonce = 0
-        self.__generateNonce__()
+        self.nonce = nounce
+        if nounce == -1:
+            self.__generateNonce__()
 
     def __generateNonce__(self):
         while (self.__checkBlock__() != 1):
@@ -91,10 +137,10 @@ class block(Chain):
     def __encode__(self):
         data = ''
         data += str(self.data)
-        data += str(self.time)
-        data += str(self.previousHash)
-        data += str(self.fitness)
-        data += str(self.nonce)
+        data += '", "' + str(self.time)
+        data += '", "' + str(self.previousHash)
+        data += '", "' + str(self.nonce) + '";; "'
+        #print(bytes(data, 'utf-8'))
         return bytes(data, 'utf-8')
 
 
@@ -105,8 +151,16 @@ def main():
     #start_chain('data1')
     #begin_chain('thats a chain')
     #make_block('dom')
-    chain1 = Chain([])
+    chain1 = Chain()
     chain1.addBlock('abc')
+    chain1.addBlock('def')
+    sending=chain1.__get_chain_bytes__()
+    #print(sending)
+    chain2 = Chain(byte=sending)
+    chain2.addBlock('ghi')
+    chain1 = chain2.best_chain(chain1)
+    sending=chain1.__get_chain_bytes__()
+    print(sending)
 
 
 # Standard boilerplate code to call the main() function to begin
