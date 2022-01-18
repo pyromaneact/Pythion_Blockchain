62998925de3b210c1da37239481c92c099ff368e
Cyber_Coo <40432224@live.napier.ac.uk>
2022-01-18 12:15:00 +0000

    update of setting up smart chain

--- serverTest.py
+++ serverTest.py
@@ -1,11 +1,21 @@
-import socket  
+import socket
+import ooBlock
 s = socket.socket()
 host = '192.168.0.77'
 port = 12345
+clients=[]
+
+chain1 = ooBlock.Chain()
+chain1.addBlock('192.168.0.13')
+chain1.addBlock('def')
+chain1.addBlock('keep trying')
+
+
 s.bind((host, port))
 s.listen(5)
 while True:
+   print('flag')
    c, addr = s.accept()     # Establish connection with client.
-   print ('Got connection from, '+ addr)
-   c.send('Thank you for connecting')
+   clients.append(addr[0])
+   c.send(chain1.__get_chain_bytes__())
    c.close()  
