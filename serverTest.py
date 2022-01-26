6f8fdbc1bdd21c2974d467b181b1a6a5b5b40757
Cyber_Coo <40432224@live.napier.ac.uk>
2022-01-26 13:23:43 +0000

    server sorted

--- serverTest.py
+++ serverTest.py
@@ -1,6 +1,40 @@
 import socket
 import ooBlock
+import threading
 
+
+def newRequest():
+    clients.append(addr[0])
+    c.send(chain1.__get_chain_bytes__())
+    c.close()
+    
+
+def newData():
+    data = c.recv(1024)
+    data = data.decode("utf-8")
+    c.close()
+    chain1.addBlock(data)
+    newsocket = socket.socket()
+    for ip in clients:  
+        newsocket.connect((ip, port))
+        newsocket.send(b'newChain')
+        newsocket.send(chain1.__get_chain_bytes__())
+
+def newchains():
+    newChainBytes = c.recv(1024)
+    c.close()
+    chain2 = Chain(byte=newChainBytes)
+    if chain1.same_chain(chain2):
+        chain1 = chain2.best_chain(chain1)
+    return chain1
+    newsocket = socket.socket()
+    for ip in clients:  
+        newsocket.connect((ip, port))
+        newsocket.send(b'newChain')
+        newsocket.send(chain1.__get_chain_bytes__())
+
+global chain1
+global  clients
 host = socket.gethostname()
 port = 12345
 clients=[]
@@ -23,36 +57,21 @@ s = socket.socket()
 s.bind((host, port))
 s.listen(5)
 
+newRequestThread = threading.Thread(target=newRequest, name='newChainThread')
+newDataThread = threading.Thread(target=newData, name='newChainThread')
+newChainThread = threading.Thread(target=newchains, name='newChainThread')
 while True:
     print('flag')
     c, addr = s.accept()
     conectionType = c.recv(1024)
     if conectionType == b'request':
-        clients.append(addr[0])
-        c.send(chain1.__get_chain_bytes__())
-        c.close()
+        newRequestThread.start
 
 
     elif conectionType == b'data':
-        data = c.recv(1024)
-        data = data.decode("utf-8")
-        c.close()
-        chain1.addBlock(data)
-        newsocket = socket.socket()
-        for ip in clients:  
-            newsocket.connect((ip, port))
-            newsocket.send(b'newChain')
-            newsocket.send(chain1.__get_chain_bytes__())
+        newDataThread.start
             
 
     elif conectionType == b'newChain':
-        newChainBytes = c.recv(1024)
-        c.close()
-        chain2 = Chain(byte=newChainBytes)
-        if chain1.same_chain(chain2):
-            chain1 = chain2.best_chain(chain1)
-        for ip in clients:  
-            newsocket.connect((ip, port))
-            newsocket.send(b'newChain')
-            newsocket.send(chain1.__get_chain_bytes__())
+        newChainThread.start
     c.close()
