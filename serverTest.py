3905ad5d8f97fc6c48430df26a5b05240ceeb561
Cyber_Coo <40432224@live.napier.ac.uk>
2022-01-31 21:10:52 +0000

    Update serverTest.py

--- serverTest.py
+++ serverTest.py
@@ -7,56 +7,60 @@ def chainNetwork(port, host):
     s = socket.socket()
     s.bind((host, port))
     s.listen(5)
-    
-    newRequestThread = threading.Thread(target=newRequest, name='newChainThread')
-    newDataThread = threading.Thread(target=newData, name='newChainThread')
-    newChainThread = threading.Thread(target=newchains, name='newChainThread')
+
+
+
     while True:
         c, addr = s.accept()
+        
+        
+        newRequestThread = threading.Thread(target=newRequest, args=(addr[0], c), name='newRequestThread')
         conectionType = c.recv(1024)
         if conectionType == b'request':
             newRequestThread.start()
     
     
         elif conectionType == b'data':
+            data = c.recv(1024)
+            data = data.decode("utf-8")
+            c.close()
+            newDataThread = threading.Thread(target=newData, args=(data,), name='newDataThread')
             newDataThread.start()
                 
     
         elif conectionType == b'newChain':
             if newDataThread.is_alive():
                 newDataThread.abort()
+            newChainBytes = c.recv(1024)
+            c.close()
+            newChainThread = threading.Thread(target=newchains, args=(newChainBytes,), name='newChainThread')
             newChainThread.start()
         c.close()
 
-def newRequest():
-    clients.append(addr[0])
+def newRequest(ip, c):
+    clients.append(ip)
     c.send(chain1.__get_chain_bytes__())
     c.close()
     
 
-def newData():
-    data = c.recv(1024)
-    data = data.decode("utf-8")
-    c.close()
+def newData(data):
     newsocket = socket.socket()
-    for ip in clients:  
-        newsocket.connect((ip, str(port)))
+    for ip in clients:
+        newsocket.connect((ip, port))
         newsocket.send(b'data')
         newsocket.send(data)
     chain1.addBlock(data)
     newsocket = socket.socket()
-    for ip in clients:  
-        newsocket.connect((ip, str(port)))
+    for ip in clients:
+        #print(ip)
+        newsocket.connect((ip, port))
         newsocket.send(b'newChain')
         newsocket.send(chain1.__get_chain_bytes__())
 
-def newchains():
-    newChainBytes = c.recv(1024)
-    c.close()
+def newchains(newChainBytes):
     chain2 = Chain(byte=newChainBytes)
     if chain1.same_chain(chain2):
         chain1 = chain2.best_chain(chain1)
-    return chain1
     newsocket = socket.socket()
     for ip in clients:  
         newsocket.connect((ip, port))
@@ -96,6 +100,7 @@ while flag == 0 :
         print(chain1.__get_chain_bytes__().decode("utf-8"))
     elif int(option) == 1:
         data=input("what data do you want to transmit: ")
+        newsocket = socket.socket()
         for ip in clients:
             newsocket.connect((ip, port))
             newsocket.send(b'data')
